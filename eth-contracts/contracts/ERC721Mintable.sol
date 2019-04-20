753c99e7dac4c05f9b44648b429058ffca916fb8
linxinzhe <linxinzhechn@qq.com>
2019-04-20 15:54:54 +0800

    ERC721Mintable contract Pausable

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -36,11 +36,40 @@ contract Ownable {
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
-//  1) create a private '_paused' variable of type bool
-//  2) create a public setter using the inherited onlyOwner modifier 
-//  3) create an internal constructor that sets the _paused variable to false
-//  4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
-//  5) create a Paused & Unpaused event that emits the address that triggered the event
+contract Pausable is Ownable{
+    //  1) create a private '_paused' variable of type bool
+    bool private _paused;
+
+    //  2) create a public setter using the inherited onlyOwner modifier
+    function pause(bool val) public onlyOwner {
+        _paused = val;
+        if (val) {
+            emit Paused(msg.sender);
+        }
+        else {
+            emit UnPaused(msg.sender);
+        }
+    }
+
+    //  3) create an internal constructor that sets the _paused variable to false
+    constructor() internal {
+        _paused = false;
+    }
+
+    //  4) create 'whenNotPaused' & 'paused' modifier that throws in the appropriate situation
+    modifier whenNotPaused(){
+        require(!_paused,"contract is paused");
+        _;
+    }
+    modifier paused(){
+        require(_paused,"contract is not paused");
+        _;
+    }
+
+    //  5) create a Paused & UnPaused event that emits the address that triggered the event
+    event Paused(address from);
+    event UnPaused(address from);
+}
 
 contract ERC165 {
     bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
